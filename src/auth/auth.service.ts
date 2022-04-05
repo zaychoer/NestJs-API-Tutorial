fbe678163fa921bd6d5557d0e8e41f2bf1fbe37c
zayyanadtn <zayyana@datasintesa.id>
2022-04-05 17:38:23 +0700

    setup

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,47 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+  async signup(dto: AuthDto) {
+    const password = await argon.hash(dto.password);
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          password,
+        },
+      });
+      delete user.password;
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Email already exists');
+        }
+      }
+      throw error;
+    }
+  }
+
+  async signin(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    if (!user) {
+      throw new ForbiddenException('Credentials incorrect');
+    }
+    const passwordMatch = await argon.verify(user.password, dto.password);
+    if (!passwordMatch) {
+      throw new ForbiddenException('Credentials incorrect');
+    }
+    delete user.password;
+    return user;
+  }
+}
